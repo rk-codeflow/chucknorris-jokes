b46b78882145d44fea3db61946d22f82afc83ef0
Raj Kiran Chaudhary <raj.chaudhary@ebpearls.com>
2023-11-21 13:39:22 +0545

    filter option added in chucknorris jokes

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 const joke = document.getElementById("joke");
 const nextBtn = document.getElementById("next-btn");
+const categorySelect = document.querySelector("select");
+
+const category = [
+  "animal",
+  "career",
+  "celebrity",
+  "dev",
+  "explicit",
+  "fashion",
+  "food",
+  "history",
+  "money",
+  "movie",
+  "music",
+  "political",
+  "religion",
+  "science",
+  "sport",
+  "travel",
+];
+
+let url = "https://api.chucknorris.io/jokes/random";
 
 async function fetchJokes() {
   try {
-    const response = await fetch("https://api.chucknorris.io/jokes/random");
+    const response = await fetch(url);
     const data = await response.json();
     console.log(data);
     joke.innerHTML = data.value;
@@ -12,5 +34,17 @@ async function fetchJokes() {
   }
 }
 
+function updateUrl(category) {
+  url = `https://api.chucknorris.io/jokes/random?category=${category}`;
+}
+
+const handleCategoryChange = () => {
+  const selectedCategory = categorySelect.value;
+  updateUrl(selectedCategory);
+  fetchJokes();
+  console.log("category changed", category);
+};
+
+categorySelect.addEventListener("change", handleCategoryChange);
 nextBtn.addEventListener("click", fetchJokes);
 fetchJokes();
